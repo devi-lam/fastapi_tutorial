dc1878e10b7e2ca0bd9d84c548aa5b871163ff23
devi-lam <devilam1807@gmail.com>
2026-03-22 12:56:28 +0800

    crud

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # HTTP METHOD GET
 from fastapi import FastAPI
 from typing import Optional
+from pydantic import BaseModel
 
 # Baris ini membuat instance aplikasi FastAPI.
 app = FastAPI()
@@ -32,5 +33,18 @@ def greet_name(name: str, age:Optional[int] = None):
 
 
 # HTTP METHOD POST
+class Student(BaseModel):
+    name: str
+    age: int
+    roll: int
+
+@app.post("/create_student")
+def create_student(student: Student):
+    return {
+        "name": student.name,
+        "age": student.age,
+        "roll": student.roll
+    }
 
 
+# CRUD OPERATION
